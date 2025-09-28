6daed30572fbcde4bff045f3332758799f80c3ae
Include caleb.fun <oasis.mystre@gmail.com>
2025-09-28 21:12:51 +0100

    feat: fix docker-compose error

--- servers/worker/src/instances.ts
+++ servers/worker/src/instances.ts
@@ -3,14 +3,20 @@ import Redis from "ioredis";
 import { web3 } from "@coral-xyz/anchor";
 import { createDB } from "@rhiva-ag/datasource";
 import { Client } from "@solana-tracker/data-api";
+
 import { getEnv } from "./env";
 
 export const redis = new Redis(getEnv("REDIS_URL"), {
   maxRetriesPerRequest: null,
 });
+console.log(
+  getEnv("RPC_URL")
+    .replace(/^https/, "wss")
+    .replace(/^http?/, "ws"),
+);
 export const connection = new web3.Connection(getEnv("RPC_URL"), {
   wsEndpoint: getEnv("RPC_URL")
-    .replace(/^https?/, "wss")
+    .replace(/^https/, "wss")
     .replace(/^http?/, "ws"),
 });
 export const solanatracker = new Client({
